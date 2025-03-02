e82f4eb8c59b4d430c4eb2b5387e0806135b82d1
Nisha <nishasaifi2004@gmail.com>
2025-03-02 11:38:58 +0530

    Update Day1_ApplyOperationstoanArray.cpp

--- Day1_ApplyOperationstoanArray.cpp
+++ Day1_ApplyOperationstoanArray.cpp
@@ -1,1 +1,34 @@
+// ---- APPROACH 2 ------- [WTIHOUT USING EXTRA SPACE]
 
+/* 
+
+  COMPLEXITY -
+  T.C = O(N)
+  S.C = O(1)
+
+*/
+
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+
+        for(int i=0 ; i < n-1  ; i++ ){
+            if(nums[i] == nums[i+1]){
+                nums[i] = nums[i]*2;
+                nums[i+1] = 0;
+            }
+        }
+
+        int idx = 0;
+        for(int i=0 ; i<n ; i++){
+            if(nums[i] != 0){
+                swap(nums[i] , nums[idx]);
+                idx++;
+            }
+        }
+        
+        return nums;
+
+    }
+};
